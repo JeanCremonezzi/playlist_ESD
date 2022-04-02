c1e31675018583f3dad50212154f01387b276757
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-04-02 16:49:18 -0300

    perf(Header): Added directives to header.h

--- header.h
+++ header.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,3 +8,5 @@
 #include "constants.h"
 #include "types.h"
 #include "prototypes.h"
+
+#endif
