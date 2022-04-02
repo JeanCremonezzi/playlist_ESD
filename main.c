a4c05ad88b1575559485e2713c0c1cae3d5a6e58
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-04-02 19:13:16 -0300

    feat(Main): Created menu loop

--- main.c
+++ main.c
@@ -4,26 +4,97 @@ int main() {
     musicsHeader* musics = init();
     playlistsHeader* playlists = initPlaylists();
 
-    insertMusic(musics, "The Darkness at The Heart of My Love", "Ghost", "Impera", 300);
-    insertMusic(musics, "Simple Man", "Lynyrd Skynyrd", "Pronounced 'Leh-'nerd 'Skin-'nerd", 356);
-    insertMusic(musics, "Kill Each Other / Live Forever", "Scars on Broadway", "Scars on Broadway", 180);
-    insertMusic(musics, "Come Join the Murder", "The White Buffalo", "Come Join the Murder", 448);
-    insertMusic(musics, "Black", "Pearl Jam", "Ten", 345);
-    insertMusic(musics, "City of Angels", "The Distillers", "Sing Sing Death House", 210);
+    int flag = 1;
+    while(flag) {
+        printf("\n-------- MUSICS MENU --------\n");
+        printf("\n> Menu:");
+        printf("\n   1. Add new music");
+        printf("\n   2. Show musics");
+        printf("\n   3. Delete music");
+        printf("\n   4. Create playlist");
+        printf("\n   5. Print playlist");
+        printf("\n   6. Shuffle playlist");
+        printf("\n   0. Sair");
 
-    //printMusics(musics);
+        printf("\n\n-> CHOOSE AN OPTION: ");
+        char optionChar;
+        fflush(stdin);
+        scanf("%c", &optionChar);
 
-    newPlaylist(playlists, musics);
-    newPlaylist(playlists, musics);
-    
-    printPlayListById(playlists, 1);
-    printPlayListById(playlists, 2);
+        if (isdigit(optionChar) == 0) {
+            printf("\n!!!!!!!!!! OPTION MUST BE A DIGIT !!!!!!!!!!\n");
+            continue;
+        }
 
-    deleteMusicById(musics, playlists, 3);
-    printMusics(musics);
+        // Char to Int
+        int option = optionChar - '0';
+        switch (option) {
+            case 0:
+                flag = 0;
+                break;
 
-    shuffleById(playlists, 1);
+            case 1:
+                printf("\n-> Music name: ");
+                char music[STRING_SIZE];
+                fflush(stdin);
+                fgets(music, STRING_SIZE, stdin);
 
-    printPlayListById(playlists, 1);
-    printPlayListById(playlists, 2);
+                printf("-> Album name: ");
+                char album[STRING_SIZE];
+                fflush(stdin);
+                fgets(album, STRING_SIZE, stdin);
+
+                printf("-> Artist name: ");
+                char artist[STRING_SIZE];
+                fflush(stdin);
+                fgets(artist, STRING_SIZE, stdin);
+
+                printf("-> Duration in seconds: ");
+                int duration;
+                scanf("%d", &duration);
+
+                insertMusic(musics, music, artist, album, duration);
+                break;
+
+            case 2:
+                printMusics(musics);
+                break;
+
+            case 3:
+                printf("\n-> Music id: ");
+                int id;
+                scanf("%d", &id);
+
+                deleteMusicById(musics, playlists, id);
+                break;
+
+            case 4:
+                newPlaylist(playlists, musics);
+                break;
+
+            case 5:
+                printPlayLists(playlists);
+
+                printf("\n\n-> Playlist id: ");
+                int playlistId;
+                scanf("%d", &playlistId);
+
+                printPlayListById(playlists, playlistId);
+                break;
+
+            case 6:
+                printPlayLists(playlists);
+
+                printf("\n\n-> Playlist id: ");
+                int playlistToShuffle;
+                scanf("%d", &playlistToShuffle);
+
+                shuffleById(playlists, playlistToShuffle);
+                break;
+            
+            default:
+                printf("\n!!!!!!!!!! INVALID OPTION !!!!!!!!!!\n");
+                break;
+        }
+    }
 }
